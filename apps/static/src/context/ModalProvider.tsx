cac21fad28b04a4b70c8cb8a7910e87580b2f994
eunsonny <eunsonnny@gmail.com>
2024-02-17 15:26:23 +0900

    Add: alias 설정

---
+++ apps/static/src/context/ModalProvider.tsx
@@ -0,0 +1,61 @@
+
+
+// import BaseModal from "@/components/modals/BaseModal/BaseModal";
+import { ReactNode, createContext, useMemo, useState } from "react";
+
+export type ModalItem = {
+  Component: unknown;
+  props?: Record<string, unknown>;
+};
+
+interface ModalContextProps {
+  modals: ModalItem[];
+  isOpen: boolean;
+}
+
+export const ModalContext = createContext<ModalContextProps>({
+  modals: [],
+  isOpen: false,
+});
+
+interface ModalDispatchContextProps {
+  open: (modal: ModalItem) => void;
+  close: (component: ModalItem["Component"]) => void;
+}
+
+export const ModalDispatchContext = createContext<ModalDispatchContextProps>({
+  open: () => {},
+  close: () => {},
+});
+
+const ModalProvider = ({ children }: { children: ReactNode }) => {
+  const [openedModals, setOpenedModals] = useState<ModalItem[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const open = (modal: ModalItem) => {
+    setOpenedModals((prev) => {
+      return [...prev, { ...modal }];
+    });
+    setIsOpen(true);
+  };
+
+  const close = (Component: ModalItem["Component"]) => {
+    setOpenedModals((prev) => {
+      return prev.filter((modal) => modal.Component !== Component);
+    });
+    setIsOpen(false);
+  };
+
+  const dispatch = useMemo(() => ({ open, close }), []);
+
+  return (
+    <ModalContext.Provider value={{ modals: openedModals, isOpen }}>
+      <ModalDispatchContext.Provider value={dispatch}>
+        <BaseModal />
+        {children}
+      </ModalDispatchContext.Provider>
+    </ModalContext.Provider>
+  );
+};
+
+export default ModalProvider;
---
+++ apps/static/src/hooks/useModal.tsx
@@ -0,0 +1,27 @@
+// import { ModalDispatchContext, ModalItem } from "@/context/ModalProvider";
+// import { ModalDispatchContext } from "../context/ModalProvider";
+import { ModalDispatchContext } from "@/context/ModalProvider";
+
+import { useContext } from "react";
+
+const useModal = () => {
+  const { open, close } = useContext(ModalDispatchContext);
+
+  const openModal = (
+    Component: ModalItem["Component"],
+    props?: ModalItem["props"]
+  ) => {
+    open({ Component, props });
+  };
+
+  const closeModal = (Component: ModalItem["Component"]) => {
+    close(Component);
+  };
+
+  return {
+    openModal,
+    closeModal,
+  };
+};
+
+export default useModal;
